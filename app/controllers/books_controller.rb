2ce1cd00e81016a375df41a5e233b588d282506c
tadeuasarro <tadeutas@gmail.com>
2021-02-08 19:02:46 -0300

    Trying some stuff

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,6 @@
 class BooksController < ApplicationController
+
+  def index
+    @book = Book.new
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+  validates :title, presence: true , uniqueness: true, length: { minimum: 4, maximum: 30 }
+  validates :category, presence: true
 end
