3f3122ff6e496e31f242c68d48b320b0a37e8a04
tadeuasarro <tadeutas@gmail.com>
2021-02-09 12:27:07 -0300

    Add index to books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,12 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = Book.all
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @books }
+    end
+  end
+
 end
